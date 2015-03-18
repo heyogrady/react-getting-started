4a1ad64834afa5f94bbf7ddab89c7a9349e10fcc
Patrick O'Grady <ogradypatrickj@gmail.com>
2015-03-18 14:32:16 -0400

    Wiring up

--- 6/app/src/js/components/FeedItem.js
+++ 6/app/src/js/components/FeedItem.js
@@ -6,17 +6,43 @@ var React = require('react');
 
 var FeedItem = React.createClass({
 
+  vote: function(newCount) {
+    this.props.onVote({
+      key: this.props.key,
+      title: this.props.title,
+      description: this.props.desc,
+      voteCount: newCount
+    });
+  },
+
+  voteUp: function() {
+    var count = parseInt(this.props.voteCount, 10);
+    var newCount = count + 1;
+    this.vote(newCount);
+  },
+
+  voteDown: function() {
+    var count = parseInt(this.props.voteCount, 10);
+    var newCount = count - 1;
+    this.vote(newCount);
+  },
+
   render: function() {
+
+    var positiveNegativeClassName = this.props.voteCount >= 0 ?
+                                    "badge badge-success" :
+                                    "badge badge-danger";
+
     return (
-      <li className="list-group-item">
-        <span className="badge badge-success">60</span>
-        <h4>Javascript is fun</h4>
-        <span>Lexical scoping FTW</span>
+      <li key={this.props.key} className="list-group-item">
+        <span className={positiveNegativeClassName}>{this.props.voteCount}</span>
+        <h4>{this.props.title}</h4>
+        <span>{this.props.desc}</span>
         <span className="pull-right">
-          <button id="up" className="btn btn-sm btn-primary">&uarr;</button>
-          <button id="down" className="btn btn-sm btn-primary">&darr;</button>
+          <button id="up" className="btn btn-sm btn-primary" onClick={this.voteUp}>&uarr;</button>
+          <button id="down" className="btn btn-sm btn-primary" onClick={this.voteDown}>&darr;</button>
         </span>
-      </li><!-- /.list-group-item -->
+      </li>
     );
   }
 
